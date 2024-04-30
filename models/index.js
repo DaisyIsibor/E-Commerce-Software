d61d22d70352ee34b81dd28a4d3001f27a78aa06
daisyisibor <daisyisibor8@gmail.com>
2024-04-30 13:18:41 -0400

    completed the relationships on index.js model folder

--- models/index.js
+++ models/index.js
@@ -4,13 +4,30 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
-// Products belongsTo Category
+// Define associations
+// Products belong to a Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id', // Foreign key in the Product table
+  onDelete: 'CASCADE',      // If a Category is deleted, associated Products are also deleted
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id', // Foreign key in the Product table
+  onDelete: 'CASCADE',      // If a Category is deleted, associated Products are also deleted
+});
 
-// Products belongToMany Tags (through ProductTag)
+// Products belong to many Tags through ProductTag
+Product.belongsToMany(Tag, {
+  through: ProductTag,      // Use the ProductTag model as the intermediary table
+  foreignKey: 'product_id', // Foreign key in the ProductTag table referencing Product
+});
 
-// Tags belongToMany Products (through ProductTag)
+// Tags belong to many Products through ProductTag
+Tag.belongsToMany(Product, {
+  through: ProductTag,   // Use the ProductTag model as the intermediary table
+  foreignKey: 'tag_id',  // Foreign key in the ProductTag table referencing Tag
+});
 
 module.exports = {
   Product,
